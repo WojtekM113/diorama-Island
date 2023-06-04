f57a9aa6fafc747ee8347c39b977bdc4fb046267
Wm <wojtek.mendlik@gmail.com>
2023-06-04 22:42:34 +0200

    fixed camera jitter

--- Assets/Scripts/PlayerScripts/MouseLook.cs
+++ Assets/Scripts/PlayerScripts/MouseLook.cs
@@ -4,28 +4,41 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
+ 
+
+ 
+
     [SerializeField] private float mouseSensitivity = 250f;
     public Transform playerTransform;
 
     float xRotation = 0f;
-  
-    void Start()
-    {
+
+    void Start() {
+
+        //We used it to make cursor locked in game 
         Cursor.lockState = CursorLockMode.Locked;
     }
 
-   
-    void LateUpdate()
-    {
+
+    void LateUpdate() {
+        
+        //mouse input
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
+        //rotating X of the player and whole object
         playerTransform.Rotate(Vector3.up * mouseX);
 
+
+        //rotating only child by using localRotation?
         xRotation -= mouseY;
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         xRotation = Mathf.Clamp(xRotation, -40f, 40f);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+
 
 
     }
+
 }
+ 
